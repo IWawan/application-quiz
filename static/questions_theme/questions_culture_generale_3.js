940ba783f9c207db6413d9b983516f4f83e5dcd3
Wawan <erwan.manier.w@gmail.com>
2025-04-28 16:01:26 +0200

    Thème culture générale #4

--- static/questions_theme/questions_culture_generale_3.js
+++ static/questions_theme/questions_culture_generale_3.js
@@ -50,4 +50,131 @@ window.loadedQuestions =
         correct: 0,
         explanation: "Paris est surnommée 'La Ville Lumière' en raison de son rôle historique en tant que centre d'art et de culture.",
     },
+    {
+        type: "image-qcm",
+        question: "Quelle mer borde la ville de Marseille ?",
+        answers: ["Mer Noire", "Mer Méditerranée", "Mer du Nord", "Mer Rouge"],
+        correct: 1,
+        explanation: "",
+        resource: "/resources/marseille.jpg"
+    },
+    {
+        type: "vrai-faux",
+        question: "La Tour de Pise est parfaitement droite.",
+        correct: 1,
+        explanation: "C'est faux, elle est célèbre pour son inclinaison."
+    },
+    {
+        type: "vrai-faux",
+        question: "La France a remporté la Coupe du Monde de football en 1998.",
+        correct: 0,
+        explanation: "C'est vrai, la France a remporté la Coupe du Monde de football en 1998 contre le Brésil."
+    },
+    {
+        type: "vrai-faux",
+        question: "La France a remporté la Coupe du Monde de football en 2006.",
+        correct: 1,
+        explanation: "C'est faux, la France a été finaliste en 2006 mais a perdu contre l'Italie aux tirs au but."
+    },
+    {
+        type: "image-qcm",
+        question: "Quel est cet animal ?",
+        answers: ["Tigre", "Léopard", "Lion", "Panthère"],
+        correct: 2,
+        explanation: "",
+        resource: "/resources/lion.jpg"
+    },
+    {
+        type: "image-vrai-faux",
+        question: "Les pandas se nourrissent principalement de bambou.",
+        correct: 0,
+        explanation: "C'est vrai, le bambou constitue l'essentiel de leur alimentation.",
+        resource: "/resources/panda.jpg"
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'élément chimique dont le symbole est 'O' ?",
+        answers: ["Or", "Ozone", "Osmium", "Oxygène"],
+        correct: 3,
+        explanation: "Le symbole 'O' correspond à l'oxygène."
+    },
+    {
+        type: "image-vrai-faux",
+        question: "La Lune est plus grande que la Terre.",
+        correct: 1,
+        explanation: "C'est faux, la Terre est bien plus grande que la Lune.",
+        resource: "/resources/lune.jpg"
+    },
+    {
+        type: "image-qcm",
+        question: "Quelle est cette célèbre tour située à Paris ?",
+        answers: ["Arc de Tromphe", "Tour Eiffel", "Tour Montparnasse", "Sacré-Cœur"],
+        correct: 2,
+        explanation: "La Tour Montparnasse est un gratte-ciel situé dans le quartier de Montparnasse à Paris.",
+        resource: "/resources/tour_montparnasse.jpg"
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'instrument utilisé pour mesurer la pression atmosphérique?",
+        answers: ["Baromètre", "Altimètre", "Thermomètre", "Chronomètre"],
+        correct: 0,
+        explanation: "",
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'océan qui borde l'est des États-Unis ?",
+        answers: ["Indien", "Pacifique", "Atlantique", "Arctique"],
+        correct: 2,
+        explanation: ""
+    },
+    {
+        type: "vrai-faux",
+        question: "L'Italie est connue pour sa forme de botte.",
+        correct: 0,
+        explanation: "C'est vrai, l'Italie a la forme d'une botte."
+    },
+    {
+        type: "image-vrai-faux",
+        question: "Les ours polaires vivent en Antarctique.",
+        correct: 1,
+        explanation: "C'est faux, ils vivent en Arctique.",
+        resource: "/resources/ours_polaire.jpg"
+    },
+    {
+        type: "image-qcm",
+        question: "Dans quel pays se trouve le Taj Mahal ?",
+        answers: ["Inde", "Pakistan", "Iran", "Indonésie"],
+        correct: 0,
+        explanation: "Le Taj Mahal est situé en Inde, à Agra.",
+        resource: "/resources/taj_mahal.jpg"
+    },
+    {
+        type: "image-qcm",
+        question: "Quel est cet animal ?",
+        answers: ["Chèvre", "Mouton", "Bélier", "Antilope"],
+        correct: 3,
+        explanation: "",
+        resource: "/resources/antilope.jpg"
+    },
+    {
+        type: "image-vrai-faux",
+        question: "Le Sahara est le plus grand désert chaud du monde.",
+        correct: 0,
+        explanation: "C'est vrai, le Sahara est le plus grand désert chaud du monde.",
+        resource: "/resources/sahara.jpg"
+    },
+    {
+        type: "image-vrai-faux",
+        question: "Les kangourous sont originaires d'Afrique.",
+        correct: 1,
+        explanation: "C'est faux, ils sont originaires d'Australie.",
+        resource: "/resources/kangourou.jpg"
+    },
+    {
+        type: "qcm",
+        question: "Quel est le plus grand océan du monde ?",
+        answers: ["Atlantique", "Indien", "Arctique", "Pacifique"],
+        correct: 3,
+        explanation: "",
+    }
 ];
---
+++ static/questions_theme/questions_culture_generale_4.js
@@ -0,0 +1,54 @@
+window.loadedQuestions =
+[
+    {
+        "type": "qcm",
+        "question": "Quel est l'animal le plus grand du monde ?",
+        "answers": ["Éléphant", "Baleine bleue", "Girafe", "Hippopotame"],
+        "correct": 1,
+        "explanation": "La baleine bleue peut atteindre 30 mètres de long."
+    },
+    {
+        "type": "image-qcm",
+        "question": "Qui a inventé l'ampoule électrique ?",
+        "answers": ["Thomas Edison", "Nikola Tesla", "Guglielmo Marconi", "Alexandre Graham Bell"],
+        "correct": 0,
+        "explanation": "Thomas Edison est célèbre pour avoir inventé la première ampoule électrique pratique en 1879.",
+        "resource": "/resources/thomas_edison.jpg"
+    },
+    {
+        "type": "image-vrai-faux",
+        "question": "Les pingouins vivent exclusivement en Antarctique.",
+        "correct": 1,
+        "explanation": "C'est faux, les pingouins vivent dans diverses régions de l'hémisphère sud, y compris en Afrique du Sud.",
+        "resource": "/resources/pingouin.jpg"
+    },
+    {
+        "type": "qcm",
+        "question": "Quel est le plus grand désert du monde ?",
+        "answers": ["Désert du Sahara", "Désert de Gobi", "Antarctique", "Désert de Kalahari"],
+        "correct": 2,
+        "explanation": "L'Antarctique est le plus grand désert du monde, avec une superficie de 14 millions de km²."
+    },
+    {
+        "type": "image-qcm",
+        "question": "Qui a peint la Nuit étoilée ?",
+        "answers": ["Claude Monet", "Henri Matisse", "Pablo Picasso", "Vincent van Gogh"],
+        "correct": 3,
+        "explanation": "La Nuit étoilée a été peinte par Vincent van Gogh en 1889.",
+        "resource": "/resources/nuit_etoilee.jpg"
+    },
+    {
+        "type": "qcm",
+        "question": "Quelle est la principale source d'énergie pour la photosynthèse ?",
+        "answers": ["L'eau", "Le soleil", "L'air", "Le sol"],
+        "correct": 1,
+        "explanation": "La photosynthèse est un processus par lequel les plantes utilisent la lumière du soleil pour produire de l'énergie."
+    },
+    {
+        "type": "image-vrai-faux",
+        "question": "Le mont Kilimandjaro se situe en Afrique.",
+        "correct": 0,
+        "explanation": "C'est vrai, le mont Kilimandjaro est situé en Tanzanie, en Afrique.",
+        "resource": "/resources/kilimandjaro.jpg"
+    },
+];
