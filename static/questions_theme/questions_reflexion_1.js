04ed6c2c99b854c6eecd6970d4e2ac52b8f50219
Wawan <erwan.manier.w@gmail.com>
2025-05-16 15:52:21 +0200

    questionss réflexion

--- static/questions_theme/questions_reflexion_1.js
+++ static/questions_theme/questions_reflexion_1.js
@@ -6,7 +6,7 @@ window.loadedQuestions =
         answers: ["Cigogne", "Flamant rose", "Girafe", "Abeille"],
         correct: 2,
         resource: "/resources/intrus_1.jpg",
-        explanation: "La girafe est le seul animal de l'image qui n'est pas un oiseau.",
+        explanation: "La girafe est le seul animal de l'image qui n'est pas volant."
     },
     {
         type: "image-memo",
@@ -14,6 +14,42 @@ window.loadedQuestions =
         answers: ["Autruche", "Éléphant", "Zèbre", "Crododile"],
         correct: 3,
         explanation: "",
-        resource: "/resources/animaux_1.jpg",
+        resource: "/resources/memo_1.jpg"
+    },
+    {
+        type: "image-memo",
+        question: "De quelle couleur était la première maison à droite ?",
+        answers: ["Jaune", "Rose", "Bleue", "Verte"],
+        correct: 2,
+        explanation: "",
+        resource: "/resources/memo_2.jpg"
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'intrus parmi ces 4 propositions ?",
+        answers: ["Chartreux", "Européen", "Carlin", "Persan"],
+        correct: 2,
+        explanation: "Le carlin désigne une race de chien, tandis que les 3 autres propositions désignent des races de chats."
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'intrus parmi ces 4 propositions ?",
+        answers: ["Voilier", "Yack", "Catamaran", "Pirogue"],
+        correct: 1,
+        explanation: "Un yack est un animal, tandis que les 3 autres propositions moyens de transport maritimes."
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'intrus parmi ces 4 couleurs ?",
+        answers: ["Vert", "Cyan", "Jaune", "Magenta"],
+        correct: 0,
+        explanation: "Le vert n'est pas une couleur primaire, on l'obtient en mélangeant le jaune et le bleu."
+    },
+    {
+        type: "qcm",
+        question: "Quel est l'intrus parmi ces 4 animaux ?",
+        answers: ["Scorpion", "Mygale", "Tique", "Fourmi"],
+        correct: 0,
+        explanation: "La fourmi est un insecte, tandis que les 3 autres propositions sont des arachnides."
     },
 ];
